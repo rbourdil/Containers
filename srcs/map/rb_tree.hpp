85599447372c8c7b1d57de56e1a3b2e9cae7eede
Raphael Bourdillat <raphael.bourdillat@outlook.f>
2023-01-28 22:24:47 +0100

    map insert and delete + iterator

--- srcs/map/rb_tree.hpp
+++ srcs/map/rb_tree.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <iostream>
 #include <stdexcept>
+#include <cstddef>
 
 #include "../alloc_help.hpp"
 
@@ -11,6 +12,7 @@ namespace ft {
 
 enum rb_tree_color { red = false, black = true };
 
+// node struct used as base type for the tree
 template <typename Key, typename T>
 struct rb_node {
 
@@ -51,6 +53,182 @@ struct rb_node {
 	node_ptr		_p;
 };
 
+template <typename Key, typename T>
+struct	rb_tree_iterator
+{
+	typedef T			value_type;
+	typedef T&			reference;
+	typedef T*			pointer;
+	typedef std::bidirectional_iterator_tag	iterator_category;
+	typedef ptrdiff_t	difference_type;
+
+	typedef	rb_node<Key, T>*	node_ptr;
+	typedef rb_tree_iterator<Key, T>	self;
+
+	public:
+
+	// constructors/destructor
+
+	rb_tree_iterator() :
+		_node()
+		{ }
+
+	rb_tree_iterator(node_ptr node) :
+		_node(node)
+		{ }
+	
+	rb_tree_iterator(const rb_tree_iterator& from) :
+		_node(from._node)
+		{ }
+
+	rb_tree_iterator&	operator=(const rb_tree_iterator& from)
+	{
+		_node = from._node;
+		return (*this);
+	}
+	
+	~rb_tree_iterator(void) { }
+
+
+	// iterator requirements
+
+	reference	operator*(void) const
+	{
+		return (_node->_val);
+	}
+
+	self&	operator++(void)
+	{
+		_node = _rb_tree_successor(_node);
+		return (*this);
+	}
+
+	// input iterator requirements
+
+	pointer	operator->(void) const
+	{
+		return (&_node->_val);
+	}
+
+	self	operator++(int)
+	{
+		self	tmp = *this;
+		_node = _rb_tree_successor(_node);
+		return (tmp);
+	}
+
+	bool	operator==(const self& comp)
+	{
+		return (_node == comp._node);
+	}
+
+	bool	operator!=(const self& comp)
+	{
+		return (_node != comp._node);
+	}
+
+	// bidirectional iterator requirements
+
+	self&	operator--(void)
+	{
+		_node = _rb_tree_predecessor(_node);
+		return (*this);
+	}
+
+	self	operator--(int)
+	{
+		self	tmp = *this;
+		_node = rb_tree_predecessor(_node);
+		return (tmp);
+	}
+
+	node_ptr	_node;
+
+};
+
+template <typename Key, typename T>
+bool	_is_null_node(rb_node<Key, T>* node)
+{
+	if (node->_p == node)
+		return (true);
+	return (false);
+}
+
+template <typename Key, typename T>
+rb_node<Key, T>*	_tree_minimum(rb_node<Key, T>* x)
+{
+	while (!_is_null_node(x->_left))
+		x = x->_left;
+	return (x);
+}
+
+template <typename Key, typename T>
+rb_node<Key, T>*	_tree_maximum(rb_node<Key, T>* x)
+{
+	while (!_is_null_node(x->_right))
+		x = x->_right;
+	return (x);
+}
+
+template <typename Key, typename T>
+rb_node<Key, T>*	_rb_tree_successor(rb_node<Key, T>* x)
+{
+	if (!_is_null_node(x->_right))
+		return (_tree_minimum(x->_right));
+	rb_node<Key, T>*	y = x->_p;
+	while (!_is_null_node(y) && x == y->_right)
+	{
+		x = y;
+		y = y->_p;
+	}
+	return (y);
+}
+
+template <typename Key, typename T>
+rb_node<Key, T>*	_rb_tree_predecessor(rb_node<Key, T>* x)
+{
+	if (!_is_null_node(x->_left))
+		return (_tree_maximum(x->_left));
+	rb_node<Key, T>*	y = x->_p;
+	while (!_is_null_node(y) && x == y->_left)
+	{
+		x = y;
+		y = y->_p;
+	}
+	return (y);
+}
+
+// struct that keeps track of the root, the begin, end, the size and the null node
+template <typename Key, typename T, typename Allocator>
+struct rb_tree_header
+{
+	typedef rb_node<Key, T>*	node_ptr;
+
+	rb_tree_header(void) :
+		_begin(NULL),
+		_size(0)
+	{
+		_null = Allocator().allocate(1);
+		Allocator().construct(_null, rb_node<Key, T>());
+		_null->_color = black;
+		_null->_left = _null;
+		_null->_right = _null;
+		_null->_p = _null;
+		_root = _null;
+	}
+
+	~rb_tree_header(void) 
+	{
+		Allocator().destroy(__builtin_addressof(*_null));
+		Allocator().deallocate(__builtin_addressof(*_null), 1);
+	}
+
+	node_ptr	_root;
+	node_ptr	_begin;
+	node_ptr	_null;
+	typename Allocator::size_type	_size;
+};
+
 template <typename Key, typename T,
 	typename Compare, typename Allocator = std::allocator<rb_node<Key, T> > >
 class rb_tree
@@ -60,31 +238,65 @@ class rb_tree
 
 	public:
 
+	typedef rb_tree_iterator<Key, T>		iterator;
+
 	// constructors/destructor
 
-	rb_tree(void) :
-		_pool(*this) { }
-	
+	rb_tree(void) { }
 
 	// rb tree operations
 
-	void	insert(const Key& key, const T& val)
+	iterator	insert(const Key& key, const T& val)
 	{
 		node_ptr	in_node;
 
-		in_node = _pool.get_node(key, val);
+		in_node = _get_node(key, val);
 		_rb_tree_insert(in_node);
+		if (_head._begin == NULL || in_node == _rb_tree_predecessor(_head._begin))
+			_head._begin = in_node;
+		_head._size++;
+		return (in_node);
 	}
+
+	void	erase(iterator pos)
+	{
+		node_ptr	pos_node = pos._node;
+		if (pos_node == _head._begin)
+			_head._begin = _rb_tree_successor(_head._begin);
+		_rb_tree_delete(pos_node);
+		_delete_node(pos_node);
+		_head._size--;
+	}
+
+	// capacity
 
 	size_type	size(void) const
 	{
 		return (_head._size);
 	}
 
+	bool	empty(void) const
+	{
+		return (_head._size == 0);
+	}
+
 	size_type	max_size(void) const
 	{
-		return (_pool._alloc.max_size());
+		return (_alloc.max_size());
 	}
+
+	// iterators
+
+	iterator	begin(void)
+	{
+		return (_head._begin);
+	}
+
+	iterator	end(void)
+	{
+		return (_head._null);
+	}
+
 
 	// helper functions 
 
@@ -117,12 +329,14 @@ class rb_tree
 
 	private:
 
+	// insert helper functions
+
+	// actual insert function
 	void	_rb_tree_insert(node_ptr z)
 	{
 		node_ptr	y = _head._null;
 		node_ptr	x = _head._root;
 
-		_head._size++;
 		while (x != _head._null)
 		{
 			y = x;
@@ -198,6 +412,131 @@ class rb_tree
 		_head._root->_color = black;
 	}
 
+	// delete helper functions
+
+	void	_rb_tree_delete(node_ptr z)
+	{
+		node_ptr	x;
+		node_ptr	y = z;
+		enum rb_tree_color	y_orig_color = y->_color;
+
+		if (z->_left == _head._null)
+		{
+			x = z->_right;
+			_transplant(z, z->_right);
+		}
+		else if (z->_right == _head._null)
+		{
+			x = z->_left;
+			_transplant(z, z->_left);
+		}
+		else
+		{
+			y = _tree_minimum(z->_right);
+			y_orig_color = y->_color;
+			x = y->_right;
+			if (y->_p == z)
+				x->_p = y;
+			else
+			{
+				_transplant(y, y->_right);
+				y->_right = z->_right;
+				y->_right->_p = y;
+			}
+			_transplant(z, y);
+			y->_left = z->_left;
+			y->_left->_p = y;
+			y->_color = z->_color;
+		}
+		if (y_orig_color == black)
+			_rb_delete_fixup(x);
+	}
+
+	void	_rb_delete_fixup(node_ptr x)
+	{
+		node_ptr	w;
+
+		while (x != _head._root && x->_color == black)
+		{
+			if (x == x->_p->_left)
+			{
+				w = x->_p->_right;
+				if (w->_color == red)
+				{
+					w->_color = black;
+					x->_p->_color = red;
+					_left_rotate(x->_p);
+					w = x->_p->_right;
+				}
+				if (w->_left->_color == black && w->_right->_color == black)
+				{
+					w->_color = red;
+					x = x->_p;
+				}
+				else
+				{
+					if (w->_right->_color == black)
+					{
+						w->_left->_color = black;
+						w->_color = red;
+						_right_rotate(w);
+						w = x->_p->_right;
+					}
+					w->_color = x->_p->_color;
+					x->_p->_color = black;
+					w->_right->_color = black;
+					_left_rotate(x->_p);
+					x = _head._root;
+				}
+			}
+			else
+			{
+				w = x->_p->_left;
+				if (w->_color == red)
+				{
+					w->_color = black;
+					x->_p->_color = red;
+					_right_rotate(x->_p);
+					w = x->_p->_left;
+				}
+				if (w->_right->_color == black && w->_left->_color == black)
+				{
+					w->_color = red;
+					x = x->_p;
+				}
+				else
+				{
+					if (w->_left->_color == black)
+					{
+						w->_right->_color = black;
+						w->_color = red;
+						_left_rotate(w);
+						w = x->_p->_left;
+					}
+					w->_color = x->_p->_color;
+					x->_p->_color = black;
+					w->_left->_color = black;
+					_right_rotate(x->_p);
+					x = _head._root;
+				}
+			}
+		}
+		x->_color = black;
+	}
+				
+
+	void	_transplant(node_ptr u, node_ptr v)
+	{
+		if (u->_p == _head._null)
+			_head._root = v;
+		else if (u == u->_p->_left)
+			u->_p->_left = v;
+		else
+			u->_p->_right = v;
+		if (v != _head._null)
+			v->_p = u->_p;
+	}
+
 	void	_left_rotate(node_ptr x)
 	{
 		node_ptr	y = x->_right;
@@ -235,43 +574,24 @@ class rb_tree
 		y->_p = x;
 	}
 
-	struct rb_tree_header
+	node_ptr	_get_node(const Key& key, const T& val)
 	{
-		rb_tree_header(void) :
-			_begin(NULL),
-			_end(NULL),
-			_size(0)
+		node_ptr	ret = _alloc.allocate(1);
+		_alloc.construct(__builtin_addressof(*ret), rb_node<Key, T>(key, val));
+		return (ret);
+	}
+
+	void	_delete_node(node_ptr x)
+	{
+		if (x != _head._null)
 		{
-			_null = Allocator().allocate(1);
-			Allocator().construct(_null, rb_node<Key, T>());
-			_null->_color = black;
-			_null->_left = _null;
-			_null->_right = _null;
-			_null->_p = _null;
-			_root = _null;
+			_alloc.destroy(__builtin_addressof(*x));
+			_alloc.deallocate(__builtin_addressof(*x), 1);
 		}
+	}
 
-		~rb_tree_header(void) 
-		{
-			Allocator().destroy(__builtin_addressof(*_null));
-			Allocator().deallocate(__builtin_addressof(*_null), 1);
-		}
-
-		void	reset(void)
-		{
-			_root = _null;
-			_begin = NULL;
-			_end = NULL;
-			_size = 0;
-		}
-
-		node_ptr	_root;
-		node_ptr	_begin;
-		node_ptr	_end;
-		node_ptr	_null;
-		size_type	_size;
-	};
-
+	/*
+	// struct that contains current allocation of nodes
 	struct	node_pool
 	{
 		node_pool(rb_tree& orig_tree) :
@@ -294,10 +614,10 @@ class rb_tree
 				_start = _alloc.allocate(1);
 				_current = _start;
 				_last = _start + 1;
-				_alloc.construct(_current, rb_node(key, val));
+				_alloc.construct(_current, rb_node<Key, T>(key, val));
 			}
 			else if (_current != _last) // enough room, just construct
-				_alloc.construct(_current, rb_node(key, val));
+				_alloc.construct(_current, rb_node<Key, T>(key, val));
 			else // need to reallocate
 			{
 				size_type	n = _last - _start;
@@ -313,7 +633,9 @@ class rb_tree
 					_tree._rb_tree_insert(new_current);
 					_alloc.destroy(__builtin_addressof(*p));
 				}
-				_alloc.construct(__builtin_addressof(*new_current), rb_node(key, val));
+				_alloc.construct(__builtin_addressof(*new_current), rb_node<Key, T>(key, val));
+				// reset the begin pointer "manually"
+				_tree._head._begin = _tree_minimum(_tree._head._root);
 
 				_alloc.deallocate(_start, n);
 
@@ -330,11 +652,12 @@ class rb_tree
 		Allocator	_alloc;
 		rb_tree&	_tree;
 	};
+	*/
 
 	// attributes
-	rb_tree_header	_head;
-	node_pool		_pool;
+	rb_tree_header<Key, T, Allocator>	_head;
 	Compare			_comp;
+	Allocator		_alloc;
 
 };
 
